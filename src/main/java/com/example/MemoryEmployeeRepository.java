f968ad4e9869333547d752cc20c2d0f967a2a595
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-01-23 11:42:33 +0100

    add tests for MemoryEmployeeRepository.java

--- src/main/java/com/example/MemoryEmployeeRepository.java
+++ src/main/java/com/example/MemoryEmployeeRepository.java
@@ -1,4 +1,33 @@
 package com.example;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MemoryEmployeeRepository {
+    List<Employee> employees;
+
+    public MemoryEmployeeRepository(){
+        this.employees = new ArrayList<>();
+    }
+
+    public MemoryEmployeeRepository(List<Employee> employees){
+        this.employees = new ArrayList<>(employees);
+
+    }
+
+    public List<Employee> findAll(){
+        return employees;
+    }
+
+    public void save(Employee employee){
+        var tempEmployees = employees.stream()
+                .filter(e -> !e.getId().equals(employee.getId()))
+                .collect(Collectors.toList());
+        tempEmployees.add(employee);
+
+        employees = tempEmployees;
+    }
+
 }
---
+++ src/test/java/com/example/MemoryEmployeeRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemoryEmployeeRepositoryTest {
+    MemoryEmployeeRepository memoryEmployeeRepository = new MemoryEmployeeRepository(List.of(new Employee("1", 100)));
+
+    @Test
+    void saveExistingEmployeeUpdatesSalary() {
+
+        var employeeOne = new Employee("1", 1000);
+
+        memoryEmployeeRepository.save(employeeOne);
+
+        assertThat(memoryEmployeeRepository.findAll().get(0).getSalary()).isEqualTo(1000.0);
+
+    }
+
+    @Test
+    void creatingNewMemoryRepoCreatesEmptyList() {
+        MemoryEmployeeRepository memoryEmployeeRepository1 = new MemoryEmployeeRepository();
+
+        assertThat(memoryEmployeeRepository1.findAll().size()).isEqualTo(0);
+    }
+}
