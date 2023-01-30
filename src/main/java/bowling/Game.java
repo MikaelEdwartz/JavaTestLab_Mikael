54c6f056075fb9dffe5a90fb305d831b24ab6ae2
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-01-30 09:21:03 +0100

    creating new game object returns zero in score

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -2,4 +2,11 @@ package bowling;
 
 public class Game {
 
+    public void roll(int pinsKnockedDown) {
+
+    }
+
+    public int score() {
+    return 0;
+    }
 }
--- src/test/java/bowling/GameTest.java
+++ src/test/java/bowling/GameTest.java
@@ -1,5 +1,22 @@
 package bowling;
 
-class GameTest {
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+class GameTest {
+    Game game = new Game();
+
+    @Test
+    void CreatingNewGameObjectReturnsNewObjectWithZeroScore() {
+
+        fillRounds(21, game, 0);
+        int score = game.score();
+
+        assertThat(score).isZero();
+    }
+
+    public void fillRounds(int rounds, Game game, int pinsKnockedDown) {
+        for (int i = 0; i < rounds; i++)
+            game.roll(pinsKnockedDown);
+    }
 }
