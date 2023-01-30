1daacd4b11a4d33a136be96817cf6be541658778
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-01-30 09:33:23 +0100

    hitting pins now adds score

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -1,12 +1,26 @@
 package bowling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    private static int MAX_ROUNDS = 21;
+    private int score;
+    private final List<Integer> rounds;
+
+    public Game() {
+        rounds = new ArrayList<>();
+    }
 
     public void roll(int pinsKnockedDown) {
-
+        if (rounds.size() < MAX_ROUNDS)
+            rounds.add(pinsKnockedDown);
     }
 
     public int score() {
-    return 0;
+        score = 0;
+            score = rounds.stream().mapToInt(Integer::intValue).sum();
+
+        return score;
     }
 }
--- src/test/java/bowling/GameTest.java
+++ src/test/java/bowling/GameTest.java
@@ -15,6 +15,41 @@ class GameTest {
         assertThat(score).isZero();
     }
 
+    @Test
+    void UsingRollIncreasesScoreByNumberOfPinsHit() {
+        int pinsToHit = 5;
+
+        game.roll(pinsToHit);
+        fillRounds(20, game, 0);
+        int score = game.score();
+
+        assertThat(score).isEqualTo(pinsToHit);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public void fillRounds(int rounds, Game game, int pinsKnockedDown) {
         for (int i = 0; i < rounds; i++)
             game.roll(pinsKnockedDown);
